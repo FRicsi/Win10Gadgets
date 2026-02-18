b3fd1c5dbc69c60fb4efa0d1b3c9030dddd395c3
FRicsi <fenyo.r@gmail.com>
2026-02-18 12:53:10 +0100

    Initial commit: WPF WebView2 Messenger widget

---
+++ MessengerWidget/App.xaml.cs
@@ -0,0 +1,149 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MessengerWidget
+{
+    public partial class App : Application
+    {
+        private TaskbarIcon? _trayIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Crash log (publishnál is hasznos) - LocalAppData alá
+            var logDir = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "MessengerWidget");
+            Directory.CreateDirectory(logDir);
+            var logPath = Path.Combine(logDir, "crash.log");
+
+            AppDomain.CurrentDomain.UnhandledException += (_, ev) =>
+            {
+                File.AppendAllText(logPath, ev.ExceptionObject?.ToString() + Environment.NewLine);
+            };
+
+            DispatcherUnhandledException += (_, ev) =>
+            {
+                File.AppendAllText(logPath, ev.Exception.ToString() + Environment.NewLine);
+                ev.Handled = true;
+            };
+
+            _trayIcon = new TaskbarIcon
+            {
+                Icon = LoadTrayIcon(),
+                ToolTipText = "Messenger Widget"
+            };
+
+            _trayIcon.TrayLeftMouseDown += (_, __) =>
+            {
+                ToggleMainWindow();
+            };
+
+            _trayIcon.ContextMenu = BuildContextMenu();
+
+            // A StartupUri miatt a MainWindow már létrejön; biztos ami biztos:
+            if (MainWindow != null)
+            {
+                MainWindow.Show();
+                MainWindow.Activate();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _trayIcon?.Dispose();
+            _trayIcon = null;
+            base.OnExit(e);
+        }
+
+        private ContextMenu BuildContextMenu()
+        {
+            var menu = new ContextMenu();
+
+            var showItem = new MenuItem { Header = "Megjelenítés" };
+            showItem.Click += (_, __) => ShowMainWindow();
+
+            var hideItem = new MenuItem { Header = "Elrejtés" };
+            hideItem.Click += (_, __) => HideMainWindow();
+
+            var exitItem = new MenuItem { Header = "Kilépés" };
+            exitItem.Click += (_, __) =>
+            {
+                if (MainWindow is MainWindow mw)
+                {
+                    mw.AllowClose = true;
+                    mw.Close();
+                }
+
+                Shutdown();
+            };
+
+            menu.Items.Add(showItem);
+            menu.Items.Add(hideItem);
+            menu.Items.Add(new Separator());
+            menu.Items.Add(exitItem);
+
+            return menu;
+        }
+
+        private void ToggleMainWindow()
+        {
+            if (MainWindow == null)
+            {
+                MainWindow = new MainWindow();
+            }
+
+            if (MainWindow.IsVisible)
+                HideMainWindow();
+            else
+                ShowMainWindow();
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow == null)
+            {
+                MainWindow = new MainWindow();
+            }
+
+            MainWindow.Show();
+            MainWindow.WindowState = WindowState.Normal;
+            MainWindow.Activate();
+        }
+
+        private void HideMainWindow()
+        {
+            MainWindow?.Hide();
+        }
+
+        private static Icon LoadTrayIcon()
+        {
+            // Embedded resource: MessengerWidget.Assets.icon.ico
+            const string resourceName = "MessengerWidget.Assets.icon.ico";
+
+            var asm = Assembly.GetExecutingAssembly();
+            using var stream = asm.GetManifestResourceStream(resourceName);
+            if (stream != null)
+            {
+                return new Icon(stream);
+            }
+
+            // Fallback: futtatási mappából (dev convenience)
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "icon.ico");
+            if (File.Exists(filePath))
+            {
+                return new Icon(filePath);
+            }
+
+            // Utolsó mentsvár: üres default icon (ne haljon meg az app)
+            return SystemIcons.Application;
+        }
+    }
+}
---
+++ MessengerWidget/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace MessengerWidget
+{
+    public partial class MainWindow : Window
+    {
+        // App.xaml.cs Exit-hez
+        public bool AllowClose { get; set; } = false;
+
+        private const string PrimaryUrl = "https://www.messenger.com/";
+        private const string FallbackUrl = "https://www.facebook.com/messages";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var userDataFolder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "MessengerWidget",
+                "WebView2");
+
+            var environment = await CoreWebView2Environment.CreateAsync(null, userDataFolder);
+            await WebView.EnsureCoreWebView2Async(environment);
+
+            SetupWebView();
+
+            // Jelenleg ez az “élő”:
+            WebView.CoreWebView2.Navigate(FallbackUrl);
+
+            // Ha majd vissza akarsz váltani messenger.com-ra:
+            // WebView.CoreWebView2.Navigate(PrimaryUrl);
+        }
+
+        private void SetupWebView()
+        {
+            var core = WebView.CoreWebView2;
+
+            core.PermissionRequested += (_, e) =>
+            {
+                if (e.PermissionKind == CoreWebView2PermissionKind.Camera ||
+                    e.PermissionKind == CoreWebView2PermissionKind.Microphone)
+                {
+                    e.State = CoreWebView2PermissionState.Allow;
+                }
+            };
+
+            core.NewWindowRequested += (_, e) =>
+            {
+                e.Handled = true;
+                core.Navigate(e.Uri);
+            };
+
+            // Fallback, ha a primary URL tényleg nem tölt be
+            core.NavigationCompleted += (_, e) =>
+            {
+                if (!e.IsSuccess)
+                {
+                    var url = WebView.Source?.ToString() ?? "";
+                    if (url.StartsWith(PrimaryUrl, StringComparison.OrdinalIgnoreCase))
+                    {
+                        core.Navigate(FallbackUrl);
+                    }
+                }
+            };
+
+            core.Settings.AreDefaultContextMenusEnabled = false;
+            core.Settings.AreDevToolsEnabled = false;
+            core.Settings.IsStatusBarEnabled = false;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            if (!AllowClose)
+            {
+                e.Cancel = true;
+                Hide();
+                return;
+            }
+
+            base.OnClosing(e);
+        }
+    }
+}
